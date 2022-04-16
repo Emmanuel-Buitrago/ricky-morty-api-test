9132dc28d23c0a5b980b879484d50f32c4a4ee2d
Emmanuel-Buitrago <buitragoemmanuel1@gmail.com>
2022-04-16 13:54:46 -0500

    2.5

--- js/fetch.js
+++ js/fetch.js
@@ -1,11 +1,29 @@
-url="https://rickandmortyapi.com/api";
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+  return parent.appendChild(el);
+}
+
+const ul = document.getElementById('authors');
+const url = 'https://rickandmortyapi.com/api';
+
 fetch(url)
-.then(function read() {
-    map{
-
-    }
-
+.then((resp) => resp.json())
+.then(function(data) {
+  let authors = data.results;
+  return authors.map(function(author) {
+    let li = createNode('li');
+    let img = createNode('img');
+    let span = createNode('span');
+    img.src = author.picture.medium;
+    span.innerHTML = `${author.name.first} ${author.name.last}`;
+    append(li, img);
+    append(li, span);
+    append(ul, li);
+  })
 })
-.catch(function() {
-
+.catch(function(error) {
+  console.log(error);
 });
